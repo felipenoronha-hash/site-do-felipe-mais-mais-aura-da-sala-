da51d863c31cc213337a07069cb378357d04c73b
felipenoronha-hash <felipe.noronha@escola.pr.gov.br>
2026-07-30 12:11:39 -0300

    alteração

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-  const botoes = document.querySelectorAll("button");
+const botoes = document.querySelectorAll("button");
 
-    botoes.forEach(function (botaoClicado){
-        botao.addEvenListener("click", botaoClicado);
-
-    function botaoClicado () {
-        console.log("fui clicado");
-        let texto = botao.querySelector("span");
-        texto.textContent++;
+    botoes.forEach(function(botao){
+        botao.addEventListener("click", botaoClicado);
+         
+        function botaoClicado () {
+            console.log("fui clicado");
+            let texto = botao.querySelector("span");
+            texto.textContent++;
     }
-});
+    });
